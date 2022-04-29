c02ae678b584845ff4464fb98171fad11ce5063e
KacperNapierski <kacper.t.napierski@gmail.com>
2022-04-29 21:37:56 +0200

    new layout, task grid TODO

---
+++ Source/App.py
@@ -0,0 +1,105 @@
+from tkinter import SOLID, Frame, Listbox, ttk
+import tkinter as tk
+from turtle import left
+import Logic as lg
+
+# ===== Varaibles =====
+
+tasks_number =3
+
+
+# ===== App Window =====
+
+App = tk.Tk()
+App.title('ToDo')
+App['bg']='#292929'
+App['padx'] = 20
+App.resizable(width=True, height=True)
+
+# ===== Layout Frames =====
+
+list_part = tk.Frame(
+    master=App,
+    width=600,
+    height=800,
+    bg='#525252'
+)
+list_part.pack(
+    side=tk.LEFT,
+    expand=True,
+    fill=tk.BOTH,
+    padx=5
+)
+
+detail_part = tk.Frame(
+    master=App,
+    width=400,
+    height=800,
+    bg='#525252'
+)
+detail_part.pack(
+    side=tk.RIGHT,
+    expand=True,
+    fill=tk.BOTH,
+    padx=5
+)
+
+
+action_frame = tk.Frame(
+    master = list_part,
+    width=600,
+    height=50,
+    relief=tk.FLAT,
+    borderwidth=2,
+    bg='#00a800'
+)
+action_frame.pack(
+    side=tk.TOP,
+    expand=False,
+    fill=tk.X
+)
+
+tasks_frame = tk.Frame(
+    master=list_part,
+    width=600,
+    height=750,
+    bg='#525252'
+)
+tasks_frame.pack(
+    side=tk.BOTTOM,
+    expand=True,
+    fill=tk.BOTH
+)
+
+# ===== Task Grid =====
+
+for i in range(tasks_number):
+    tasks_frame.columnconfigure(1, weight=1, minsize=400)
+    
+    task= tk.Frame(
+        master=tasks_frame,
+        relief=tk.FLAT,
+        borderwidth=2,
+        width=600
+    )
+    task.pack(
+        side=tk.TOP,
+        # expand=True,
+        fill=tk.BOTH
+    )
+    task.grid(row=i, column=1)
+    label = tk.Label(
+        master=task,
+        text =f'task {i}',
+        bg='#525252',
+        width=100,
+        height=1
+    )
+    label.pack(
+        side=tk.TOP,
+        expand=True,
+        fill=tk.BOTH
+    )
+
+
+App.mainloop()
--- Source/Logic.py
+++ Source/Logic.py
@@ -1,18 +1,28 @@
 import tkinter as tk
 from unicodedata import name
-
 from tables import Description
+import App
 
+class task_data:
+    def __init__(
+        self,
+        task_name,
+        task_description,
+        task_date,
+        task_time
+    ):
 
-class list_slot(tk.Frame):
-    def __init__(self, task_name, task_description, task_date, task_time, master=tk.Frame):
         self.task_name = task_name
         self.task_description = task_description
         self.task_date = task_date
         self.task_time = task_time
+        super().__init__()
+        
+
+class task_slot:
+    def __init__(self, master=App.tasks_frame):
         super().__init__(master)
         self.pack()
 
-    def hover():
 
-        
+# def Add_task():
